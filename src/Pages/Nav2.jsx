64e053e2f2b55a3274b09f15faffde4c1a917bbc
Payal-gurunani <152200601+Payal-gurunani@users.noreply.github.com>
2024-06-23 21:48:54 +0530

    Update Nav2.jsx

--- src/Pages/Nav2.jsx
+++ src/Pages/Nav2.jsx
@@ -4,7 +4,7 @@ function Nav2() {
   return (
     <div className='w-full flex h-full flex-wrap align-middle m-20 mt-30 '>
         <h1 id='khush'>
-           Welcome to the world technoloy
+           Welcome to the world technoloy of khushveer
         </h1>
 
     </div>
